8477c1e388cc6dc9e2edf9f0599900eac82e5228
ayan-goel <agoel320@gatech.edu>
2025-02-02 15:15:17 -0500

    parts 5 and 6

--- VAASA/settings.py
+++ VAASA/settings.py
@@ -124,3 +124,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 STATICFILES_DIRS = [
     BASE_DIR / 'VAASA/static/',
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
