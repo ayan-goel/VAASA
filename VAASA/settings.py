f0993e1fe679522fe3fdbfa09545a3755198b6f9
Vatsal Dwivedi <vatsal.dwivedi27@gmail.com>
2025-02-03 11:19:28 -0500

    Part 7 Vatsal Dwivedi

--- VAASA/settings.py
+++ VAASA/settings.py
@@ -81,8 +81,8 @@ WSGI_APPLICATION = "VAASA.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
