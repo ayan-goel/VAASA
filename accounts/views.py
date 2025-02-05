52dc6e1b25969f8c8ed5c4369415972d0e35f653
24parida <68526448+24parida@users.noreply.github.com>
2025-02-05 15:15:13 -0500

    finished login + starting logout

--- accounts/views.py
+++ accounts/views.py
@@ -32,7 +32,7 @@ def signup(request):
         form = CustomUserCreationForm(request.POST, error_class=CustomErrorList)
         if form.is_valid():
             form.save()
-            return redirect('home.index')
+            return redirect('accounts.login')
         else:
             template_data['form'] = form
             return render(request, 'accounts/signup.html',
