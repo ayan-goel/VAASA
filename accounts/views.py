213ed90fda8f9058f60a313d1d6b027dfbba034f
24parida <68526448+24parida@users.noreply.github.com>
2025-02-05 15:12:11 -0500

    added login page

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, CustomErrorList
+from django.contrib.auth import login as auth_login, authenticate
+def login(request):
+    template_data = {}
+    template_data['title'] = 'Login'
+    if request.method == 'GET':
+        return render(request, 'accounts/login.html',
+            {'template_data': template_data})
+    elif request.method == 'POST':
+        user = authenticate(
+            request,
+            username = request.POST['username'],
+            password = request.POST['password']
+        )
+        if user is None:
+            template_data['error'] = 'The username or password is incorrect.'
+            return render(request, 'accounts/login.html',
+                {'template_data': template_data})
+        else:
+            auth_login(request, user)
+            return redirect('home.index')
 def signup(request):
     template_data = {}
     template_data['title'] = 'Sign Up'
@@ -9,7 +29,7 @@ def signup(request):
         return render(request, 'accounts/signup.html',
             {'template_data': template_data})
     elif request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST, error_class=CustomErrorList)
         if form.is_valid():
             form.save()
             return redirect('home.index')
