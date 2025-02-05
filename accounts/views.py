262b4be77f9e3e95eb2ec99101a7e4a5094d7fee
24parida <68526448+24parida@users.noreply.github.com>
2025-02-05 14:52:01 -0500

    added_urls

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.urls import path
+from django.contrib.auth.forms import UserCreationForm
+def signup(request):
+    template_data = {}
+    template_data['title'] = 'Sign Up'
+    if request.method == 'GET':
+        template_data['form'] = UserCreationForm()
+        return render(request, 'accounts/signup.html',
+            {'template_data': template_data})
 # Create your views here.
